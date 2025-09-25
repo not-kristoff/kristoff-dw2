9b4e730e39875902c78b88b56fbf1b32f1e2e872
not-kristoff <166732665+not-kristoff@users.noreply.github.com>
2025-09-25 20:37:13 +0000

    commits stuff for ryan

---
+++ src/components/markdown/sample-content.md
@@ -0,0 +1,15 @@
+# My Awesome Markdown File
+
+This is a file I'm writing in **Markdown**. I'm going to use the content from this inside another file in my project.
+
+## Why I Like Markdown
+
+- It's easy to read and write
+- I can edit it in a plain text editor
+- It supports _most_ of the same stuff as HTML
+
+## Why I Don't Like Markdown
+
+(empty)
+
+I love markdown!
---
+++ src/data/items.js
@@ -0,0 +1,14 @@
+const item = {
+    title:"Title",
+    quote:"This is the quote"};
+
+
+import quote1image from "../assets/image-sample.jpg"
+    
+
+export const items = [
+    {title:'quote 1',quote:'this is a quote', image:quote1image},
+    {title:'quote 2',quote:'this is a quote', image:quote1image},
+    {title:'quote 3',quote:'this is a quote', image:quote1image}];
+
+
